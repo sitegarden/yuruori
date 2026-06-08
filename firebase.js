033ef394af6d715209b6039de43560f5e1325626
Lane <121688300+AlexLevel0@users.noreply.github.com>
2026-06-08 14:50:13 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKXkN33lf_Fv4haTsartMtFfnUo5nQLZg",
+  authDomain: "yuruori-lane.firebaseapp.com",
+  projectId: "yuruori-lane",
+  storageBucket: "yuruori-lane.firebasestorage.app",
+  messagingSenderId: "935807368483",
+  appId: "1:935807368483:web:f9e9d95e5b803ac4aca096"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
