bd5764f5f21e8c557ebfd3c0607ac4e799d94670
Lane <121688300+AlexLevel0@users.noreply.github.com>
2026-06-09 21:31:01 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import {
+  getAuth,
+  GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCKXkN33lf_Fv4haTsartMtFfnUo5nQLZg",
@@ -13,3 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
